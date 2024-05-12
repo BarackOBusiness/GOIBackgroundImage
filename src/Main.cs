a15bd8f887dbec08b9a18accee79fc044934f998
BarackOBusiness <nichjoh21@gmail.com>
2024-05-12 18:48:50 -0400

    Setup building and project layout

---
+++ src/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ImageBackground;
+
+[BepInPlugin("goi.ext.imagebackground", "Background Image", "0.1.0")]
+public class ImageBackground : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo("Background Image is loaded!");
+    }
+}
